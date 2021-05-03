8fba5895a8cb60ee7a49f9762822ef71aeb12958
gaston223 <g.coulibalyy@gmail.com>
2021-05-03 20:12:47 +0200

    Select with Query Builder, CommentTableSeeder

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,35 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
-    dump(factory(App\Comment::class, 3)->create());
+    // Get All users
+    $users = DB::table('users')->get();
+
+    // Get All users and only Email column
+    $usersEmail = DB::table('users')->pluck('email');
+
+    //Get Email Value from by name
+    $user = DB::table('users')->where('name', 'Chelsey Grady')->value('email');
+
+    // Get a single user by Id
+    $user = DB::table('users')->find(1);
+
+    //Get comments and  only content column
+    $commentsContent = DB::table('comments')->select('content as comment_content')->get();
+
+    //Get distinct user_id who comments
+    $userIds = DB::table('comments')->select('user_id')->distinct()->get();
+
+    // Get the max user_id who comments
+    $result = DB::table('comments')->max('user_id');
+
+    //Get the sum of userId who comments
+    $result = DB::table('comments')->sum('user_id');
+
+    //Check if comment content exists in DB return boolean
+    $result = DB::table('comments')->where('content', 'content')->exists();
+    $result = DB::table('comments')->where('content', 'content')->doesntExist();
+
+    dump($usersEmail);
 
     return view('welcome');
 });
